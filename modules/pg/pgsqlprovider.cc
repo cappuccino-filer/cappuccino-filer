6c61d21150b14cbbfd69eb97a43bf337d24a2fd1
Xinya Zhang <xinyazhang@utexas.edu>
2016-07-24 23:37:32 -0500

    pg: fix SQL syntax

--- modules/pg/pgsqlprovider.cc
+++ modules/pg/pgsqlprovider.cc
@@ -54,11 +54,11 @@ INSERT INTO tracking_table(uuid,tracking) VALUES(:1,1) RETURNING trID;
 			SQLINDEX(volume, CREATE),
 R"zzz(
 CREATE TABLE IF NOT EXISTS vol_#id_inode_table (inode BIGINT PRIMARY KEY, size
-BIGINT NOT NULL, hash BLOB(32) NULL, mtime_sec BIGINT NULL, mtime_nsec BIGINT
-NULL, last_check DATETIME NULL, ack BOOLEAN);
-CREATE UNIQUE INDEX idx_inode ON vol_#id_inode_table (inode);
+BIGINT NOT NULL, hash BYTEA NULL, mtime_sec BIGINT NULL, mtime_nsec BIGINT
+NULL, last_check TIMESTAMP WITH TIME ZONE NULL, ack BOOLEAN);
+CREATE UNIQUE INDEX vol_#id_idx_inode ON vol_#id_inode_table (inode);
 CREATE TABLE IF NOT EXISTS vol_#id_dentry_table (dnode BIGINT NOT NULL, name VARCHAR(255) NOT NULL, inode BIGINT NOT NULL, ack BOOLEAN, PRIMARY KEY (dnode, name) );
-CREATE UNIQUE INDEX idx_dentry ON vol_#id_dentry_table (dnode, name);
+CREATE UNIQUE INDEX vol_#id_idx_dentry ON vol_#id_dentry_table (dnode, name);
 )zzz"
 		},
 		{
@@ -79,15 +79,15 @@ DELETE FROM vol_#id_inode_table WHERE ack = false;
 			SQLINDEX(volume, UPSERT_DENTRY),
 R"zzz(
 INSERT INTO vol_#id_dentry_table (dnode, name, inode, ack) VALUES (:d, :n, :i,
-1) ON CONFLICT ON CONFLICT vol_#id_dentry_table_pkey DO UPDATE SET inode=EXCLUDED.inode, ack=1;
+true) ON CONFLICT ON CONSTRAINT vol_#id_dentry_table_pkey DO UPDATE SET inode=EXCLUDED.inode, ack = true;
 )zzz"
 		},
 		{
 			SQLINDEX(volume, UPSERT_INODE),
 R"zzz(
 INSERT INTO vol_#id_inode_table (inode, size, mtime_sec, mtime_nsec, ack)
-VALUES (:1, :2, :3, :4, 1) ON CONFLICT ON CONFLICT vol_#id_inode_table_pkey DO UPDATE SET size=EXCLUDED.size,
-mtime_sec=EXCLUDED.mtime_sec, mtime_nsec=EXCLUDED.mtime_nsec, ack=1;
+VALUES (:1, :2, :3, :4, true) ON CONFLICT ON CONSTRAINT vol_#id_inode_table_pkey DO UPDATE SET size=EXCLUDED.size,
+mtime_sec=EXCLUDED.mtime_sec, mtime_nsec=EXCLUDED.mtime_nsec, ack = true;
 )zzz"
 		},
 	};
