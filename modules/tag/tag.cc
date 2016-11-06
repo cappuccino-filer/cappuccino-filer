7926de9fa098d900354251a7ff08ed8c8d46ca5e
Xinya Zhang <xinyazhang@utexas.edu>
2016-11-05 20:48:35 -0500

    tag: refactor FileTagActionFab.

--- modules/tag/tag.cc
+++ modules/tag/tag.cc
@@ -1,4 +1,5 @@
 #include "tag.h"
+#include "filetag.h"
 #include <soci/soci.h>
 #include <database.h>
 #include <database_query_table.h>
@@ -264,7 +265,7 @@ namespace {
 
 #define NAIVE_FAB(T) [](shared_ptree pt)->ActionPtr { return std::make_unique<T>(pt); }
 
-	std::unordered_map<string, fab_function_t> fabmap = {
+	std::unordered_map<string, fab_function_t> tagfabmap = {
 		{"create", NAIVE_FAB(CreateTag) }
 		, {"list", NAIVE_FAB(ListTag) }
 		, {"name2id", NAIVE_FAB(LocateTag) }
@@ -274,16 +275,34 @@ namespace {
 #endif
 	};
 
+	std::unordered_map<string, fab_function_t> filetagfabmap = {
+#if 0 // Disable them first.
+		{"assign", NAIVE_FAB(AssigTagToFile) }
+		, {"tagrel", NAIVE_FAB(TagRelation) }
+#endif
+	};
+
 #undef NAIVE_FAB
 
+	std::unique_ptr<TagAction>
+	find_and_construct(const std::unordered_map<string, fab_function_t>& fabmap,
+			   ptree pt)
+	{
+		string cat = pt.get("cat", "");
+		auto iter = fabmap.find(cat);
+		if (iter == fabmap.end())
+			return std::make_unique<DummyTagAction>(pt);
+		else
+			return iter->second(pt);
+	}
 };
 
 std::unique_ptr<TagAction> TagActionFab::fab(shared_ptree pt)
 {
-	string cat = pt.get("cat", "");
-	auto iter = fabmap.find(cat);
-	if (iter == fabmap.end())
-		return std::make_unique<DummyTagAction>(pt);
-	else
-		return iter->second(pt);
+	return find_and_construct(tagfabmap, pt);
+}
+
+std::unique_ptr<TagAction> FileTagActionFab::fab(shared_ptree pt)
+{
+	return find_and_construct(filetagfabmap, pt);
 }
